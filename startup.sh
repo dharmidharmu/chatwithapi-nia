e1424a6dbe02185221b5d10be35ac74d0a737e97
calvinjose-ritaantony <calvinjose.ritaantony@in.bosch.com>
2025-06-19 17:38:22 +0530

    Python Module (pydantic) version conflict fix

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ python -m venv customgptapp2
 source customgptapp2/bin/activate
 python -m pip install --upgrade pip
 echo "Installing Requirements"
-pip install -r requirements.txt
+pip install --upgrade -r requirements.txt
 echo "Installing uvicorn separately"
 pip install fastapi uvicorn python-multipart
 echo "The uvicorn version installed is:"
